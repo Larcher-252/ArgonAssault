d3df364df253252771582f3b8960a3ee93600d3d
Void7 <limen191100@gmail.com>
2022-03-29 18:37:30 +0300

    Add pitch, yaw and roll rotation on movement
    Updated PlayerController.cs:
       1. Change rotation on position to pitch and yaw.
       2. Change rotation on input to pitch and roll.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,12 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float controllSpeed = 30f;
     [SerializeField] float xRange = 20f;
     [SerializeField] float yMax = 7f;
+    [SerializeField] float positionPitchFactor = 20f;
+    [SerializeField] float ControlPitchFactor = 10f;
+    [SerializeField] float positionYawFactor = 20f;
+    [SerializeField] float ControlRollFactor = 10f;
+    float xThrow = 0f;
+    float yThrow = 0f;
 
     private void Update()
     {
@@ -16,14 +22,24 @@ public class PlayerController : MonoBehaviour
 
     void ProcessRotation()
     {
-        transform.localRotation = Quaternion.Euler(-30f, 30f, 0f);
+        float pitchToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchToThrow = yThrow * ControlPitchFactor;
+
+        float yawToPosition = transform.localPosition.x * -positionYawFactor;
+
+        float rollToThrow = xThrow * -ControlRollFactor;
+
+        float pitch = pitchToPosition + pitchToThrow;
+        float yaw = yawToPosition;
+        float roll = rollToThrow;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 
     // Update is called once per frame
     void ProcessTranslation()
     {
-        float xThrow = Input.GetAxis("Horizontal");
-        float yThrow = Input.GetAxis("Vertical");
+        xThrow = Input.GetAxis("Horizontal");
+        yThrow = Input.GetAxis("Vertical");
 
         float xOffset = xThrow * Time.deltaTime * controllSpeed;
         float xNewPos = Mathf.Clamp(transform.localPosition.x + xOffset, -xRange, xRange);
